ba2f9344a4106690e168499bf47b5a5ffa56d9e5
Korver Huang <korver2017@gmail.com>
2019-04-13 17:47:45 +0800

    Edit project component & reset App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,6 @@
 <template>
   <div id="app" class="mt-5">
-
-    <div class="col-3 input-group mb-3 mx-auto">
-
-      <input v-model.trim="stashProjectName" type="text" class="form-control" placeholder="" aria-label="Project name" aria-describedby="button-addon2">
-
-      <div class="input-group-append">
-        <button @click="addProject ()" class="btn btn-outline-success" type="button" id="button-addon2">Add project</button>
-      </div>
-
-    </div>
-      
-      <template v-for="project in projects">
-
-        <li class="list-group-item list-group-item-primary mt-5" v-for="(name, i) in project.projectName">
-
-          <h3>Project： {{ name }}</h3>
-          <button class="mt-3 btn btn-warning" @click="editProjectName (i)">Edit Project Name</button>
-
-          <div class="col-6 input-group my-5 mx-auto">
-            <input v-model.trim="newIssue" type="text" class="form-control" placeholder="" aria-label="" aria-describedby="button-addon2">
-
-            <div class="input-group-append">
-              <button @click="addIssue (i)" class="btn btn-primary" type="button" id="button-addon2">Add issue</button>
-            </div>
-            <br />
-          </div>
-          
-          <issue :issuesData="project.issueGroup[i]" :position="i" />
-          
-        </li>
-      </template>
-
+    <h1>Home Component</h1>
   </div>
 </template>
 
@@ -56,10 +25,6 @@
 </style>
 
 <script>
-  import Issue from '@/views/issue.vue';
-
-
-  import { eventBus } from "@/main.js";
 
 
   export default {
@@ -74,7 +39,6 @@
      *
      */
     components: {
-      Issue,
     },
 
 
@@ -84,74 +48,7 @@
 
     data () {
       return {
-        projects: [],
-        stashProjectName: 'kk',
-        projectData: {
-          projectName: ['Project-1', 'Project-2'],
-          issueGroup: [[
-            {
-              title: '#1 Issue-1',
-              issueContent: 'Issue-content',
-              comments: ['Comment-1'],
-            },
-            {
-              title: '#1.1',
-              issueContent: 'Issue-content',
-              comments: ['Comment-1'],
-            },
-          ], [
-            {
-              title: '#2',
-              issueContent: '66-content',
-              comments: ['66-1'],
-            },
-          ]],
-        },
-        newIssue: '',
       }
     },
-
-    
-    methods: {
-      addProject () {
-        let $vmc = this;
-        if ($vmc.stashProjectName === '') return;
-
-        $vmc.projectData.projectName.push ($vmc.stashProjectName);
-        $vmc.projects.push (0, 0, $vmc.projectData);
-        $vmc.stashProjectName = '';
-      },
-
-      editProjectName (i) {
-        let $vmc = this;
-
-        $vmc.projectData.projectName.splice (i, 1, 'qq');
-      },
-
-      addIssue (i) {
-        let $vmc = this;
-        
-        if ($vmc.projectData.issueGroup[i] === undefined) {
-          $vmc.projectData.issueGroup[i] = new Array ();
-
-          $vmc.projectData.issueGroup[i].splice (0, 0, {
-            title: $vmc.newIssue,
-            issueContent: '',
-            comments: [$vmc.projectData.issueGroup[i]],
-          });
-
-          console.log ($vmc.projectData.issueGroup[i]);
-        } else {
-          $vmc.projectData.issueGroup[i].splice (0, 0, {
-            title: $vmc.newIssue,
-            issueContent: '',
-            comments: [],
-          });
-
-          console.log ($vmc.projectData.issueGroup[i]);
-        }
-      }
-    },
-
   }
 </script>
--- src/views/project/project.vue
+++ src/views/project/project.vue
@@ -13,20 +13,20 @@
   </div>
 </template>
 
+<style lang="scss">
+</style>
+
 <script>
-  import Issue from "@/views/issue.vue";
+  import Issue from '@/views/issue.vue';
+
 
   import { eventBus } from "@/main.js";
 
-  export default {
-    
 
-    /**
-     *
-     * Component name
-     *
-     */
-    name: 'project',
+  export default {
+
+
+    name: 'Home',
 
 
     /**
@@ -39,27 +39,80 @@
     },
 
 
-    props: ['projects'],
-
-
     created () {
-      // eventBus.$on ('newIssue', issue => {
-      //   console.log (issue);
-
-
-      // });
     },
 
 
+    data () {
+      return {
+        projects: [],
+        stashProjectName: 'kk',
+        projectData: {
+          projectName: ['Project-1', 'Project-2'],
+          issueGroup: [[
+            {
+              title: '#1 Issue-1',
+              issueContent: 'Issue-content',
+              comments: ['Comment-1'],
+            },
+            {
+              title: '#1.1',
+              issueContent: 'Issue-content',
+              comments: ['Comment-1'],
+            },
+          ], [
+            {
+              title: '#2',
+              issueContent: '66-content',
+              comments: ['66-1'],
+            },
+          ]],
+        },
+        newIssue: '',
+      }
+    },
+
+    
     methods: {
-      editProjectName (i) {
-        this.projects[i].projectName = 'qq';
+      addProject () {
+        let $vmc = this;
+        if ($vmc.stashProjectName === '') return;
+
+        $vmc.projectData.projectName.push ($vmc.stashProjectName);
+        $vmc.projects.push (0, 0, $vmc.projectData);
+        $vmc.stashProjectName = '';
       },
+
+      editProjectName (i) {
+        let $vmc = this;
+
+        $vmc.projectData.projectName.splice (i, 1, 'qq');
+      },
+
+      addIssue (i) {
+        let $vmc = this;
+        
+        if ($vmc.projectData.issueGroup[i] === undefined) {
+          $vmc.projectData.issueGroup[i] = new Array ();
+
+          $vmc.projectData.issueGroup[i].splice (0, 0, {
+            title: $vmc.newIssue,
+            issueContent: '',
+            comments: [$vmc.projectData.issueGroup[i]],
+          });
+
+          console.log ($vmc.projectData.issueGroup[i]);
+        } else {
+          $vmc.projectData.issueGroup[i].splice (0, 0, {
+            title: $vmc.newIssue,
+            issueContent: '',
+            comments: [],
+          });
+
+          console.log ($vmc.projectData.issueGroup[i]);
+        }
+      }
     },
 
   }
 </script>
-
-<style scoped>
-  
-</style>
