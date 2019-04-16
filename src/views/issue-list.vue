95834c5f1a1e78709d67e893f04413d0c778951d
Korver Huang <korver2017@gmail.com>
2019-04-16 13:45:13 +0800

    Add label-bug

--- src/views/issue-list.vue
+++ src/views/issue-list.vue
@@ -3,28 +3,36 @@
     <h1 class="my-4">{{ title }}</h1>
 
         <div class="list-group">
-          <a @click="showContent = !showContent" class="list-group-item list-group-item-action text-left">{{ issueTitle }}<span v-if="feature === true" class="badge badge-success text-right">Feature</span></a>
+          <a @click="showContent = !showContent" class="list-group-item list-group-item-action text-left">{{ issueTitle }}
+            <span v-if="feature === true" class="badge badge-success text-right">
+              Feature
+            </span>
+            <span v-if="bug === true" class="badge badge-danger text-right">
+              bug
+            </span>
+          </a>
 
           <template v-if="showContent">
           <div class="text-left my-5 border border-primary">
             1. 新增 Issue <br />
             2. 新增 label
-            <span v-if="feature === true" class="badge badge-success text-right">Feature</span>
+            <span v-if="feature === true" class="badge badge-success text-right">
+              Feature
+            </span>
+            <span v-if="bug === true" class="badge badge-danger text-right">
+              bug
+            </span>
           </div>
 
           <div>
             <button @click="feature = !feature" class="btn btn-success">Feature</button>
+            <button @click="bug = !bug" class="btn btn-danger">Bug</button>
           </div>
 
           </template>
 
         </div>
 
-        
-
-        
-
-        <!-- <Issue v-if="showContent" @labelState="changeLabel" /> -->
       </div>
     
 
@@ -49,18 +57,13 @@
         title: 'Issue-List Component',
         issueTitle: '#1-Issue',
         showContent: false,
-        
         feature: false,
+        bug: false,
       }
     },
 
 
     methods: {
-      changeLabel (val) {
-        console.log (val);
-
-        this.feature = val;
-      }
     },
   }
 </script>
