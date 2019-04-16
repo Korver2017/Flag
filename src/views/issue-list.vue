19a19dfba732dce5337a6127bf48012e43c0c66c
Korver Huang <korver2017@gmail.com>
2019-04-16 12:14:56 +0800

    Reset issue-list component structure

--- src/views/issue-list.vue
+++ src/views/issue-list.vue
@@ -2,19 +2,32 @@
   <div class="container my-4">
     <h1 class="my-4">{{ title }}</h1>
 
-    <template v-for="issueTitle in issuesTitle">
+        <div class="list-group">
+          <a @click="showContent = !showContent" class="list-group-item list-group-item-action text-left">{{ issueTitle }}<span v-if="feature === true" class="badge badge-success text-right">Feature</span></a>
 
-      <router-link to="/issue" active-class="" class="list-group">
-        <button @click="showContent = !showContent" class="list-group-item list-group-item-action">{{ issueTitle }}</button>
-      </router-link>
-      
-    </template>
+          <template v-if="showContent">
+          <div class="text-left my-5 border border-primary">
+            1. 新增 Issue <br />
+            2. 新增 label
+            <span v-if="feature === true" class="badge badge-success text-right">Feature</span>
+          </div>
 
+          <div>
+            <button @click="feature = !feature" class="btn btn-success">Feature</button>
+          </div>
+
+          </template>
+
+        </div>
+
+        
+
+        
+
+        <!-- <Issue v-if="showContent" @labelState="changeLabel" /> -->
+      </div>
     
-    <router-view v-if="!showContent" />
-    
 
-  </div>
 </template>
 
 <script>
@@ -34,10 +47,21 @@
     data () {
       return {
         title: 'Issue-List Component',
-        issuesTitle: ['Issue 1.1', 'Issue 1.2', 'Issue 1.2.1'],
+        issueTitle: '#1-Issue',
         showContent: false,
+        
+        feature: false,
       }
-    }
+    },
+
+
+    methods: {
+      changeLabel (val) {
+        console.log (val);
+
+        this.feature = val;
+      }
+    },
   }
 </script>
 
--- src/views/issue.vue
+++ src/views/issue.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>Issue Component</h1>
+    <span v-if="feature === true">feature</span>
+
+    <button @click="labelState" class="btn btn-success text-right">Feature</button>
   </div>
 </template>
 
@@ -11,6 +14,20 @@
     name: 'issue',
 
 
+    data () {
+      return {
+        feature: false,
+      }
+    },
+
+
+    methods: {
+      labelState () {
+        this.feature = !this.feature;
+
+        this.$emit ('changeLabel', this.feature);
+      }
+    },
   }
 </script>
 
