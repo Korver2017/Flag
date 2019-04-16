bca5466b305fecb5aabefd43780b73fae57c53ac
Korver Huang <korver2017@gmail.com>
2019-04-16 10:20:00 +0800

    Add issue-component & set router-link / router-view

--- src/views/issue-list.vue
+++ src/views/issue-list.vue
@@ -2,26 +2,40 @@
   <div class="container my-4">
     <h1 class="my-4">{{ title }}</h1>
 
-    <div v-for="issueTitle in issuesTitle" class="list-group">
-      <button type="button" class="list-group-item list-group-item-action">
-        {{ issueTitle }}
-      </button>
-    </div>
+    <template v-for="issueTitle in issuesTitle">
+
+      <router-link to="/issue" active-class="" class="list-group">
+        <button @click="showContent = !showContent" class="list-group-item list-group-item-action">{{ issueTitle }}</button>
+      </router-link>
+      
+    </template>
+
+    
+    <router-view v-if="!showContent" />
+    
 
   </div>
 </template>
 
 <script>
+  import Issue from "@/views/issue.vue";
+
   export default {
     
 
     name: 'issue-list',
 
 
+    components: {
+      Issue,
+    },
+
+
     data () {
       return {
         title: 'Issue-List Component',
-        issuesTitle: ['Issue 1.1', 'Issue 1.2', 'Issue 1.2.1']
+        issuesTitle: ['Issue 1.1', 'Issue 1.2', 'Issue 1.2.1'],
+        showContent: false,
       }
     }
   }
---
+++ src/views/issue.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Issue Component</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+
+    name: 'issue',
+
+
+  }
+</script>
+
+<style scoped>
+  
+</style>
