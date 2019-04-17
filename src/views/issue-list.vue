617113009f8bd7626a8999f8313246c174f7aaad
Korver Huang <korver2017@gmail.com>
2019-04-17 17:09:06 +0800

    Add Fn to close/open issue & edit issue-title

--- src/views/issue-list.vue
+++ src/views/issue-list.vue
@@ -9,67 +9,94 @@
       </div>
     </div>
 
-    <template v-for="(issueData, index) in issuesData">
+    <div v-for="(issueData, index) in issuesData">
+      <!-- <div> -->
 
-      <div class="list-group">
-      
+        <div class="list-group text-white">
 
-        <button :disabled="issueData.issueOpened === false" @click="changeStatus(index)" class="mt-3 list-group-item list-group-item-action text-left bg-dark text-white">
-          {{ issueData.title }}
-          <span v-if="issueData.issueOpened === false"> (Closed)</span>
-        </button>
-        <button v-if="issueData.issueOpened === true" class="btn btn-danger" @click="issueData.issueOpened = !issueData.issueOpened">Close issue</button>
-        <button v-else class="btn btn-success" @click="issueData.issueOpened = !issueData.issueOpened">Open issue</button>
+          <button :disabled="issueData.issueOpened === false" @click="changeStatus(index)" class="mt-3 list-group-item list-group-item-action text-left bg-dark text-white">
+            {{ issueData.title }}
+            <span v-if="issueData.issueOpened === false"> (Closed)</span>
+          </button>
 
-        <button :disabled="issueData.issueOpened === false" class="list-group-item list-group-item-action" v-if="issueData.showContent">
-          <div>
+          <li :class="{ 'disabled': issueData.issueOpened === false }" class="list-group-item list-group-item-action bg-secondary" v-if="issueData.showContent">
+            <div>
+              <p class="text-left text-white">{{ issueData.content }}</p>
 
-            {{ issueData.content }}
-            <button v-if="editing === false" class="btn btn-warning" @click="editing = !editing">Edit</button>
-            <template v-else>
+              <div v-if="issueData.contentEditing === true" class="col-6 mx-auto input-group mb-3">
+                <input @keyup.enter="updateContent(index)" v-model="issueData.stashContent" type="text" class="form-control" placeholder="New content" aria-describedby="button-addon2">
+                <div class="input-group-append">
+                  <button @click="updateContent(index)" class="btn btn-warning" type="button" id="button-addon2">Update content</button>
+                </div>
+              </div>
 
-              <div class="input-group mb-3">
+              <div v-if="issueData.titleEditing === true" class="col-6 mx-auto input-group mb-3">
+                <input @keyup.enter="updateTitle(index)" v-model="issueData.stashTitle" type="text" class="form-control" placeholder="New title" aria-describedby="button-addon2">
+                <div class="input-group-append">
+                  <button @click="updateTitle(index)" class="btn btn-warning" type="button" id="button-addon2">Update title</button>
+                </div>
+              </div>
+
+              <template>
+                <button class="mx-2 btn btn-warning" @click="issueData.contentEditing = !issueData.contentEditing">Edit content</button>
+                <button class="mx-2 btn btn-warning" @click="issueData.titleEditing = !issueData.titleEditing">Edit title</button>
+              </template>
+
+              <!-- <div v-if="contentEditing === true" class="input-group mb-3">
                 <input @keyup.enter="updateContent(index)" v-model="stashContent" type="text" class="form-control" placeholder="New content" aria-describedby="button-addon2">
                 <div class="input-group-append">
                   <button @click="updateContent(index)" class="btn btn-outline-secondary" type="button" id="button-addon2">Update content</button>
                 </div>
               </div>
 
-            </template>
-
-            <template :labels="issueData.issueLabels">
-
-              <template v-for="labels in issueData.issueLabels">
-                {{ labels }}
-                <!-- <span v-if="label === true" class="badge badge-success text-right">
-                  {{ label }}
-                </span> -->
-              </template>
-
-
-              <!-- <span v-if="issueData.labels.feature === true" class="badge badge-success text-right">
-                feature
-              </span>
-
-              <span v-if="issueData.labels.bug === true" class="badge badge-danger text-right">
-                Bug
-              </span> -->
-
-            </template>
+              <div v-if="issueData.titleEditing === true" class="input-group mb-3">
+                <input @keyup.enter="updateTitle(index)" v-model="issueData.stashTitle" type="text" class="form-control" placeholder="New title" aria-describedby="button-addon2">
+                <div class="input-group-append">
+                  <button @click="updateTitle(index)" class="btn btn-outline-secondary" type="button" id="button-addon2">Update title</button>
+                </div>
+              </div> -->
             
-          </div>
+
+              <!-- <template :labels="issueData.issueLabels">
+
+                <template v-for="labels in issueData.issueLabels">
+                  {{ labels }}
+                   <span v-if="label === true" class="badge badge-success text-right">
+                    {{ label }}
+                  </span> 
+                </template>
 
 
-        </button>
+                 <span v-if="issueData.labels.feature === true" class="badge badge-success text-right">
+                  feature
+                </span>
+
+                <span v-if="issueData.labels.bug === true" class="badge badge-danger text-right">
+                  Bug
+                </span> 
+
+              </template> -->
+              
+            </div>
 
 
-      </div>
+          </li>
 
-      <template v-if="issueData.showContent" v-for="(label, key) in labels">
-        <button @click="changeLabelState(index, key)" class="mt-3 btn btn-outline-primary">{{ key }}</button>
-      </template>
 
-    </template>
+        </div>
+
+        <div class="mt-3">
+        <button v-if="issueData.issueOpened === true" class="btn btn-danger" @click="issueData.issueOpened = !issueData.issueOpened">Close issue</button>
+        <button v-else class="btn btn-success" @click="issueData.issueOpened = !issueData.issueOpened">Open issue</button>
+        </div>
+
+        <!-- <button :disabled="issueData.issueOpened === false" class="list-group-item list-group-item-action" v-if="issueData.showContent">
+        </button> -->
+
+      <!-- </div> -->
+
+
+    </div>
 
 
   </div>
@@ -90,24 +117,29 @@
         issuesData: [
           {
             title: '#1',
+            titleEditing: false,
             showContent: false,
-            content: 'content-1 description',
+            content: '#1 - Lorem...',
             issueLabels: {},
-            // labels: {feature: false, bug: false},
+            labels: {feature: false, bug: false},
             issueOpened: true,
+            stashTitle: '',
+            stashContent: '',
+            contentEditing: false,
           }, {
             title: '#2',
+            titleEditing: false,
             showContent: false,
-            content: 'content-2 description',
+            content: '#2 - Lorem...',
             issueLabels: {},
-            // labels: {feature: false, bug: false},
+            labels: {feature: false, bug: false},
             issueOpened: true,
+            stashContent: '',
+            contentEditing: false,
           }
         ],
-        labels: {feature: false, bug: false},
+        // labels: {feature: false, bug: false},
         newIssue: '',
-        stashContent: '',
-        editing: false,
       }
     },
 
@@ -121,14 +153,16 @@
 
 
       addIssue () {
-        // let issueTitle = this.newIssue;
 
         this.issuesData.push ({
           title: this.newIssue,
+          titleEditing: false,
           showContent: false,
           content: '尚無資訊',
           labels: {feature: false, bug: false},
           issueOpened: true,
+          stashContent: '',
+          contentEditing: false,
         });
 
         this.newIssue = '';
@@ -142,9 +176,17 @@
 
 
       updateContent (index) {
-        this.issuesData[index].content = this.stashContent;
-        this.stashContent = '';
-        this.editing = false;
+        this.issuesData[index].content = this.issuesData[index].stashContent;
+        this.issuesData[index].stashContent = '';
+        this.issuesData[index].contentEditing = false;
+      },
+
+
+      updateTitle (index) {
+        this.issuesData[index].title = this.issuesData[index].stashTitle;
+        this.issuesData[index].stashTitle = '';
+        this.issuesData[index].titleEditing = false;
+
       }
 
     },
