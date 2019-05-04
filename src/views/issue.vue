f0809583552768a11e837162fe97c8468f65cb89
Korver Huang <korver2017@gmail.com>
2019-05-04 15:43:35 +0800

    Add issue-content & title

--- src/views/issue.vue
+++ src/views/issue.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container my-4">
-    <h3>Issue Component</h3>
-    <h3>{{ issueId }}</h3>
+    <!-- <h3>Issue Component</h3> -->
+    <h3 class="text-left">{{ title }}</h3>
+
+    <div>{{ content }}</div>
 
   </div>
     
@@ -18,11 +20,43 @@
     name: 'issue',
 
 
+    data () {
+      return {
+        content: '',
+        title: '',
+      }
+    },
+
+
     computed: {
       issueId () {
         return this.$route.params.issueId; 
       }
     },
+
+
+    mounted () {
+
+      let $vmc = this;
+
+      const Issue = Parse.Object.extend ("Issue");
+      let query = new Parse.Query (Issue);
+      query.get ($vmc.issueId)
+        .then (resp => {
+          let content = resp.get ('content');
+          $vmc.content = content;
+
+          let title = resp.get ('name');
+          $vmc.title = title;
+        })
+
+
+      
+    },
+
+
+    watch: {
+    }
   }
 </script>
 
