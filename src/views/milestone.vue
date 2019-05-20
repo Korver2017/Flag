19aa4c72229152571a8c389949acf8912e840a56
Korver Huang <korver2017@gmail.com>
2019-05-20 10:51:31 +0800

    Add delete-milestone Fn

--- src/views/milestone.vue
+++ src/views/milestone.vue
@@ -59,76 +59,6 @@
     
     <ul v-for="(mile, index) in milestones" class="list-group list-group-flush">
 
-      <li v-if="mile.mileOpened === false && showOpened === false" class="py-5 text-left list-group-item">
-
-        <div v-if="milestones[index].mileEditing === false">
-
-          <router-link :to="{ name: 'milestone-detail', params: { mileId: mile.mileId, mileTitle: mile.title }}" class="text-decoration-none" tag="a" active-class="active">
-            <h3 class="my-3">{{ mile.title }}</h3>
-          </router-link>
-
-          <div class="row">
-
-            <div class="col-6">
-
-              <p v-if="milestones[index].dueDate !== ''">Due Date by {{ milestones[index].dueDate }}</p>
-              <p v-if="milestones[index].mileDesc !== ''">{{ milestones[index].mileDesc }}</p>
-
-            </div>
-
-            <div class="col-6">
-
-              <div class="mx-auto row">
-                <p class="mx-2">{{ mile.percentage }}% complete</p>
-                <p class="mx-2">{{ mile.open }} open</p>
-                <p class="mx-2">{{ mile.closed }} closed</p>
-              </div>
-
-              <div class="progress">
-                <div class="progress-bar bg-success" role="progressbar" :style="{ width: mile.percentage + '%'}" aria-valuemin="0" aria-valuemax="100"></div>
-              </div>
-
-            </div>
-
-            <div class="text-right mb-5 ml-auto mr-5">
-
-              <div class="text-right mt-3">
-
-                <button v-if="milestones[index].mileEditing === false" class="mr-3 btn btn-success" @click.prevent="milestones[index].mileEditing = true">Edit</button>
-                <button v-if="milestones[index].mileOpened === true && milestones[index].mileEditing === false" class="mr-3 btn btn-warning" @click.prevent="closeMilestone (index)">Close</button>
-                <button v-else-if="milestones[index].mileOpened === false && milestones[index].mileEditing === false" class="mr-3 btn btn-primary" @click.prevent="reopenMilestone (index)">Reopen</button>
-                <button v-if="milestones[index].mileEditing === false" class="mr-3 btn btn-danger">Delete</button>
-
-              </div>
-
-            </div>
-
-          </div>
-
-        </div>
-
-        <div v-else class="col-8 mx-auto form-group text-left">
-
-          <label>Milestone Title</label>
-          <input v-model.trim="milestones[index].title" class="mb-3 form-control">
-
-          <label>Due Date (Optional)</label>
-          <date-picker v-model="milestones[index].dueDate" :config="options"></date-picker>
-
-          <label class="mt-3" for="description">Description</label>
-          <textarea v-model="milestones[index].mileDesc" class="form-control mb-3" rows="10" id="description"></textarea>
-
-          <div class="text-center">
-
-            <button class="mx-1 btn btn-success" @click.prevent="updateMile (index)">Update</button>
-            <button class="mx-1 btn btn-danger" @click.prevent="cancelEditing (index)">Cancel</button>
-
-          </div>
-
-        </div>
-
-      </li>
-
       <li v-if="mile.mileOpened === true && showOpened === true" class="py-5 text-left list-group-item">
 
         <div v-if="milestones[index].mileEditing === false">
@@ -167,7 +97,7 @@
                 <button v-if="milestones[index].mileEditing === false" class="mr-3 btn btn-success" @click.prevent="milestones[index].mileEditing = true">Edit</button>
                 <button v-if="milestones[index].mileOpened === true && milestones[index].mileEditing === false" class="mr-3 btn btn-warning" @click.prevent="closeMilestone (index)">Close</button>
                 <button v-else-if="milestones[index].mileOpened === false && milestones[index].mileEditing === false" class="mr-3 btn btn-primary" @click.prevent="reopenMilestone (index)">Reopen</button>
-                <button v-if="milestones[index].mileEditing === false" class="mr-3 btn btn-danger">Delete</button>
+                <button v-if="milestones[index].mileEditing === false" class="mr-3 btn btn-danger" @click="deleteMile (index)">Delete</button>
 
               </div>
 
@@ -197,7 +127,75 @@
 
         </div>
 
-        
+      </li>
+
+      <li v-if="mile.mileOpened === false && showOpened === false" class="py-5 text-left list-group-item">
+
+        <div v-if="milestones[index].mileEditing === false">
+
+          <router-link :to="{ name: 'milestone-detail', params: { mileId: mile.mileId, mileTitle: mile.title }}" class="text-decoration-none" tag="a" active-class="active">
+            <h3 class="my-3">{{ mile.title }}</h3>
+          </router-link>
+
+          <div class="row">
+
+            <div class="col-6">
+
+              <p v-if="milestones[index].dueDate !== ''">Due Date by {{ milestones[index].dueDate }}</p>
+              <p v-if="milestones[index].mileDesc !== ''">{{ milestones[index].mileDesc }}</p>
+
+            </div>
+
+            <div class="col-6">
+
+              <div class="mx-auto row">
+                <p class="mx-2">{{ mile.percentage }}% complete</p>
+                <p class="mx-2">{{ mile.open }} open</p>
+                <p class="mx-2">{{ mile.closed }} closed</p>
+              </div>
+
+              <div class="progress">
+                <div class="progress-bar bg-success" role="progressbar" :style="{ width: mile.percentage + '%'}" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+
+            </div>
+
+            <div class="text-right mb-5 ml-auto mr-5">
+
+              <div class="text-right mt-3">
+
+                <button v-if="milestones[index].mileEditing === false" class="mr-3 btn btn-success" @click.prevent="milestones[index].mileEditing = true">Edit</button>
+                <button v-if="milestones[index].mileOpened === true && milestones[index].mileEditing === false" class="mr-3 btn btn-warning" @click.prevent="closeMilestone (index)">Close</button>
+                <button v-else-if="milestones[index].mileOpened === false && milestones[index].mileEditing === false" class="mr-3 btn btn-primary" @click.prevent="reopenMilestone (index)">Reopen</button>
+                <button v-if="milestones[index].mileEditing === false" @click="deleteMile (index)" class="mr-3 btn btn-danger">Delete</button>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div v-else class="col-8 mx-auto form-group text-left">
+
+          <label>Milestone Title</label>
+          <input v-model.trim="milestones[index].title" class="mb-3 form-control">
+
+          <label>Due Date (Optional)</label>
+          <date-picker v-model="milestones[index].dueDate" :config="options"></date-picker>
+
+          <label class="mt-3" for="description">Description</label>
+          <textarea v-model="milestones[index].mileDesc" class="form-control mb-3" rows="10" id="description"></textarea>
+
+          <div class="text-center">
+
+            <button class="mx-1 btn btn-success" @click.prevent="updateMile (index)">Update</button>
+            <button class="mx-1 btn btn-danger" @click.prevent="cancelEditing (index)">Cancel</button>
+
+          </div>
+
+        </div>
 
       </li>
 
@@ -475,6 +473,32 @@
               .then (resp => {
                 $vmc.cancelEditing (index);
               })
+          })
+      },
+
+
+      deleteMile (index) {
+        let $vmc = this;
+        let mileId = $vmc.milestones[index].mileId;
+        let Mile = Parse.Object.extend ('Milestone');
+        let query = new Parse.Query (Mile);
+
+        query.get (mileId)
+          .then (resp => {
+            // Now let's update it with some new data. In this case, only cheatMode and score
+            // will get sent to the cloud. playerName hasn't changed.
+            if (confirm ('確定要刪除此 Milestone 嗎？')) {
+              resp.destroy ()
+                .then (resp => {
+                  $vmc.showMile ();
+                // The object was deleted from the Parse Cloud.
+              }, (error) => {
+                // The delete failed.
+                // error is a Parse.Error with an error code and message.
+              });
+            }
+            
+            else return;
           })
       },
 
