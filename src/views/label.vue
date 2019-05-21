144236d8e1bd5e650e81427d433203d896a2d7ff
Korver Huang <korver2017@gmail.com>
2019-05-21 22:57:05 +0800

    List labels in label-component

--- src/views/label.vue
+++ src/views/label.vue
@@ -1,12 +1,36 @@
 <template>
   <div>
     <h1>Label Component</h1>
+    <h3>{{ labels }}</h3>
   </div>
 </template>
 
 <script>
+  import Parse from 'parse';
+
   export default {
-    
+    data() {
+      return {
+        labels: [],
+      }
+    },
+    mounted () {
+      let $vmc = this;
+      const Label = Parse.Object.extend ('Label');
+      const query = new Parse.Query (Label);
+      let ary = [];
+      query.notEqualTo ('title', '');
+      query.find ()
+        .then (resp => {
+          for (let i = 0; i < resp.length; i++) {
+            let object = resp[i];
+            ary.push (object.get ('title'));
+            alert (object.id + ' - ' + object.get ('title'));
+          }
+        });
+      
+      $vmc.labels = ary;
+    }
   }
 </script>
 
