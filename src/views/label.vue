6a3f864400112c4304c8132a25a495d759f3d86a
Korver Huang <korver2017@gmail.com>
2019-05-22 00:00:44 +0800

    Add style & new-label input form

--- src/views/label.vue
+++ src/views/label.vue
@@ -1,7 +1,31 @@
 <template>
-  <div>
-    <h1>Label Component</h1>
-    <h3>{{ labels }}</h3>
+  <div class="my-5 container">
+
+    <h1 class="mb-5">Label Component</h1>
+
+    <button v-if="addingLabel === false" class="btn btn-success" @click="addingLabel = true">New Label</button>
+
+
+      <div v-if="addingLabel === true" class="d-flex justify-content-center row form-group text-left">
+        <div class="col-3">
+          <label for="labelName">Label Name</label>
+          <input v-model="labelName" type="text" class="form-control" aria-describedby="button-addon2" id="labelName">
+        </div>
+        <div class="col-5">
+          <label for="labelDesc">Description</label>
+          <input v-model="labelDesc" type="text" class="form-control" aria-describedby="button-addon2" id="labelDesc">
+        </div>
+        <div class="d-flex align-items-end">
+          <button class="mr-3 btn btn-success" @click="newLabel">Create Label</button>
+          <button class="mr-3 btn btn-danger" @click="cancelLabel">Cancel</button>
+        </div>
+      </div>
+
+
+    <ul class="my-3 list-group list-group-flush text-left">
+      <li v-for="label in labels" class="py-3 list-group-item">{{ label }}</li>
+    </ul>
+
   </div>
 </template>
 
@@ -12,6 +36,9 @@
     data() {
       return {
         labels: [],
+        addingLabel: false,
+        labelName: '',
+        labelDesc:'',
       }
     },
     mounted () {
@@ -25,12 +52,23 @@
           for (let i = 0; i < resp.length; i++) {
             let object = resp[i];
             ary.push (object.get ('title'));
-            alert (object.id + ' - ' + object.get ('title'));
           }
         });
       
       $vmc.labels = ary;
-    }
+    },
+    methods: {
+      newLabel () {
+        let $vmc = this;
+        console.log ($vmc.labelName, $vmc.labelDesc);
+      },
+      cancelLabel () {
+        let $vmc = this;
+        $vmc.labelName = '';
+        $vmc.labelDesc = '';
+        $vmc.addingLabel = false;
+      }
+    },
   }
 </script>
 
