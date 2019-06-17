7d5f0b511b3cdc773c232dc47354410f04cce844
Korver Huang <korver2017@gmail.com>
2019-06-17 15:35:59 +0800

    Seperate data to show opened / closed issue in my-issue component

--- src/views/my-issue.vue
+++ src/views/my-issue.vue
@@ -43,17 +43,15 @@
 
         <ul class="list-group list-group-flush">
 
-          <button class="d-flex justify-content-between text-left my-1 list-group-item border-0">屬於該用戶儲存庫的 <span>{{ openedCount }}</span></button>
-          <button class="d-flex justify-content-between text-left my-1 list-group-item border-0">指派給您的 <span>{{ assignToMe }}</span></button>
-          <button class="d-flex justify-content-between text-left my-1 list-group-item border-0">由您建立的 <span>{{ createByMe }}</span></button>
+          <button @click="switchTo = 'myIssue', showOpened = true" class="d-flex justify-content-between text-left my-1 list-group-item border-0">屬於該用戶儲存庫的 <span>{{ issueOpenedCount }}</span></button>
+          <button @click="switchTo = 'assignToMe', showOpened = true" class="d-flex justify-content-between text-left my-1 list-group-item border-0">指派給您的 <span>{{ assignToMeOpened }}</span></button>
+          <button @click="switchTo = 'createdByMe', showOpened = true" class="d-flex justify-content-between text-left my-1 list-group-item border-0">由您建立的 <span>{{ createdByMeOpened }}</span></button>
 
         </ul>
 
         <hr />
 
         <ul v-for="openedIssue in openedIssueList" class="list-group list-group-flush">
-
-          <h3>{{ openedIssue }}</h3>
 
           <button class="d-flex justify-content-between text-left my-1 list-group-item border-0">
             {{ openedIssue.orgName }}/{{ openedIssue.proName }}
@@ -65,20 +63,70 @@
 
       <div class="col-9">
 
-        <div class="row">
+
+
+
+
+
+
+
+
+
+
+
+
+        <div v-if="switchTo === 'myIssue'" class="row">
 
           <div class="btn-group" role="group" aria-label="Basic example">
             <button @click="showOpened = true" type="button" class="btn btn-outline-secondary">
-              {{ openedCount }} 個開啟中
+              {{ issueOpenedCount }} 個開啟中
             </button>
             <button @click="showOpened = false" type="button" class="btn btn-outline-secondary">
-              {{ closedCount }} 個已關閉
+              {{ issueClosedCount }} 個已關閉
             </button>
           </div>
 
         </div>
 
-        <div class="mt-3 list-group list-group-flush " v-if="issues.length >= 1">
+        <div v-else-if="switchTo === 'assignToMe'" class="row">
+
+          <div class="btn-group" role="group" aria-label="Basic example">
+            <button @click="showOpened = true" type="button" class="btn btn-outline-secondary">
+              {{ assignToMeOpened }} 個開啟中
+            </button>
+            <button @click="showOpened = false" type="button" class="btn btn-outline-secondary">
+              {{ assignToMeClosed }} 個已關閉
+            </button>
+          </div>
+
+        </div>
+
+        <div v-else-if="switchTo === 'createdByMe'" class="row">
+
+          <div class="btn-group" role="group" aria-label="Basic example">
+            <button @click="showOpened = true" type="button" class="btn btn-outline-secondary">
+              {{ createdByMeOpened }} 個開啟中
+            </button>
+            <button @click="showOpened = false" type="button" class="btn btn-outline-secondary">
+              {{ createdByMeClosed }} 個已關閉
+            </button>
+          </div>
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <div class="mt-3 list-group list-group-flush " v-if="issues.length >= 1 && switchTo === 'myIssue'">
 
           <template v-for="issue in issues">
 
@@ -122,6 +170,118 @@
             
         </div>
 
+
+
+
+
+
+
+
+        <div class="mt-3 list-group list-group-flush " v-if="switchTo === 'assignToMe'">
+
+          <template v-for="issue in assignToMeIssues">
+
+            <div v-if="issue.issueOpened === true && showOpened === true" class="text-left list-group-item">
+
+              <div class="row my-3">
+
+                <span class="d-flex align-items-center rounded py-2 px-3 bg-light-theme mr-3">{{ issue.orgName }}/{{ issue.proName }}</span>
+
+                <router-link v-if="issues.length > 0" :to="{ name: 'issue', params: { issueId: issue.issueId, orgId: issue.orgId, proId: issue.proId }}" tag="button" type="button" class="btn">
+
+                  <h4>{{ issue.issueName }}</h4>
+
+                </router-link>
+
+              </div>
+
+              <div>由 {{ issue.creator }} 建立</div>
+
+            </div>
+
+            <div v-if="issue.issueOpened === false && showOpened === false" class="text-left list-group-item">
+
+              <div class="row my-3">
+
+                <span class="d-flex align-items-center rounded py-2 px-3 bg-light-theme mr-3">{{ issue.orgName }}/{{ issue.proName }}</span>
+
+                <router-link v-if="issues.length > 0" :to="{ name: 'issue', params: { issueId: issue.issueId, orgId: issue.orgId, proId: issue.proId }}" tag="button" type="button" class="btn">
+
+                  <h4>{{ issue.issueName }}</h4>
+
+                </router-link>
+
+              </div>
+
+              <div>由 {{ issue.creator }} 建立</div>
+
+            </div>
+
+          </template>
+            
+        </div>
+
+
+
+
+
+
+
+
+
+
+        <div class="mt-3 list-group list-group-flush " v-if="switchTo === 'createdByMe'">
+
+          <template v-for="issue in createdByMeIssues">
+
+            <div v-if="issue.issueOpened === true && showOpened === true" class="text-left list-group-item">
+
+              <div class="row my-3">
+
+                <span class="d-flex align-items-center rounded py-2 px-3 bg-light-theme mr-3">{{ issue.orgName }}/{{ issue.proName }}</span>
+
+                <router-link v-if="issues.length > 0" :to="{ name: 'issue', params: { issueId: issue.issueId, orgId: issue.orgId, proId: issue.proId }}" tag="button" type="button" class="btn">
+
+                  <h4>{{ issue.issueName }}</h4>
+
+                </router-link>
+
+              </div>
+
+              <div>由 {{ issue.creator }} 建立</div>
+
+            </div>
+
+            <div v-if="issue.issueOpened === false && showOpened === false" class="text-left list-group-item">
+
+              <div class="row my-3">
+
+                <span class="d-flex align-items-center rounded py-2 px-3 bg-light-theme mr-3">{{ issue.orgName }}/{{ issue.proName }}</span>
+
+                <router-link v-if="issues.length > 0" :to="{ name: 'issue', params: { issueId: issue.issueId, orgId: issue.orgId, proId: issue.proId }}" tag="button" type="button" class="btn">
+
+                  <h4>{{ issue.issueName }}</h4>
+
+                </router-link>
+
+              </div>
+
+              <div>由 {{ issue.creator }} 建立</div>
+
+            </div>
+
+          </template>
+            
+        </div>
+
+
+
+
+
+
+
+        
+
       </div>
 
     </div>
@@ -141,11 +301,16 @@
         orgs: '',
         issues: '',
         showOpened: true,
-        openedCount: 0,
-        closedCount: 0,
-        assignToMe: 0,
-        createByMe: 0,
         openedIssueList: '',
+        switchTo: 'myIssue',
+        issueOpenedCount: 0,
+        issueClosedCount: 0,
+        assignToMeOpened: 0,
+        assignToMeClosed: 0,
+        createdByMeOpened: 0,
+        createdByMeClosed: 0,
+        assignToMeIssues: '',
+        createdByMeIssues: '',
       }
     },
 
@@ -161,9 +326,11 @@
     },
 
     mounted () {
-      this.showIssue ();
-      this.showAssignee ();
+      // this.showIssue ();
+      this.showMyIssue ();
+      // this.showAssignee ();
       this.showCreatedByMe ();
+      this.showAssigneToMe ();
     },
     
     methods: {
@@ -171,12 +338,78 @@
         let $vmc = this;
         let Issue = Parse.Object.extend ('Issue');
         let query = new Parse.Query (Issue);
+        let ary = [];
 
+        // $vmc.switchTo = 'createdByMe';
         query.equalTo ('creator', $vmc.user.username);
         query.find ()
           .then (resp => {
-            $vmc.createByMe = resp.length;
+            let all = resp.length;
+            let opened = 0;
+
+            for (let i = 0; i < all; i ++) {
+              let obj = {};
+              let object = resp[i];
+
+              obj.issueId = object.id;
+              obj.orgId = object.get ('orgId');
+              obj.proId = object.get ('proId');
+              obj.proName = object.get ('proName');
+              obj.orgName = object.get ('orgName');
+              obj.issueName = object.get ('name');
+              obj.creator = object.get ('creator');
+              obj.issueOpened = object.get ('issueOpened');
+
+              if (obj.issueOpened === true) {
+                opened += 1;
+              }
+              
+              ary.push (obj);
+            }
+
+            $vmc.createdByMeOpened = opened;
+            $vmc.createdByMeClosed = all - opened;
+            $vmc.createdByMeIssues = ary;
           });
+      },
+
+      showAssigneToMe () {
+        let $vmc = this;
+        let Issue = Parse.Object.extend ('Issue');
+        let query = new Parse.Query (Issue);
+        let ary = [];
+
+        // $vmc.switchTo = 'assignToMe';
+        query.equalTo ('assigneeId', $vmc.user.userId);
+        query.find ()
+          .then (resp => {
+            let opened = 0;
+            let all = resp.length;
+            
+            for (let i = 0; i < all; i ++) {
+              let obj = {};
+              let object = resp[i];
+              obj.issueId = object.id;
+              obj.orgId = object.get ('orgId');
+              obj.proId = object.get ('proId');
+              obj.proName = object.get ('proName');
+              obj.orgName = object.get ('orgName');
+              obj.issueName = object.get ('name');
+              obj.creator = object.get ('creator');
+              obj.issueOpened = object.get ('issueOpened');
+
+              if (obj.issueOpened === true) {
+                opened += 1;
+              }
+              
+              ary.push (obj);
+            }
+
+            $vmc.assignToMeOpened = opened;
+            $vmc.assignToMeClosed = all - opened;
+            $vmc.assignToMeIssues = ary;
+          })
+
       },
 
       showAssignee () {
@@ -198,96 +431,128 @@
           });
           
       },
-      
-      showIssue () {
 
+      showMyIssue () {
         let $vmc = this;
-        let Org = Parse.Object.extend ('Organization');
-        let query = new Parse.Query (Org);
+        let Issue = Parse.Object.extend ('Issue');
+        let query = new Parse.Query (Issue);
         let ary = [];
-        query.equalTo ('memberId', $vmc.user.userId);
+
+        // $vmc.switchTo = 'myIssue';
+        query.notEqualTo ('objectId', '');
         query.find ()
           .then (resp => {
-            for (let i = 0; i < resp.length; i ++) {
+            let opened = 0;
+            let all = resp.length;
+
+            for (let i = 0; i < all; i ++) {
+              let obj = {};
               let object = resp[i];
-              ary.push (object.id);
+              obj.issueId = object.id;
+              obj.orgId = object.get ('orgId');
+              obj.proId = object.get ('proId');
+              obj.proName = object.get ('proName');
+              obj.orgName = object.get ('orgName');
+              obj.issueName = object.get ('name');
+              obj.creator = object.get ('creator');
+              obj.issueOpened = object.get ('issueOpened');
+
+              if (obj.issueOpened === true) {
+                opened += 1;
+              }
+              
+              ary.push (obj);
             }
 
-            return ary;
-          })
-          .then (resp => {
-            let ary = [];
-            let orgId = resp;
-            let Project = Parse.Object.extend ('Project');
-            let query = new Parse.Query (Project);
-
-            query.containedIn ('orgId', resp);
-            query.find ()
-              .then (resp => {
-                for (let i = 0; i < resp.length; i ++) {
-                  let object = resp[i];
-                  ary.push (object.id)
-                }
-
-                return ary;
-              })
-              .then (resp => {
-                let ary = [];
-                let arry = [];
-                let Issue = Parse.Object.extend ('Issue');
-                let query = new Parse.Query (Issue);
-
-                query.containedIn ('proId', resp);
-                query.find ()
-                  .then (resp => {
-                    
-                    for (let i = 0; i < resp.length; i ++) {
-                      let obj = {};
-                      let object = resp[i];
-                      obj.issueId = object.id;
-                      obj.orgId = object.get ('orgId');
-                      obj.proId = object.get ('proId');
-                      obj.proName = object.get ('proName');
-                      obj.orgName = object.get ('orgName');
-                      obj.issueName = object.get ('name');
-                      obj.issueOpened = object.get ('issueOpened');
-                      obj.creator = object.get ('creator');
-
-                      if (obj.issueOpened === true) {
-                        $vmc.openedCount += 1;
-                        arry.push (obj);
-                      } else {
-                        $vmc.closedCount += 1;
-                      }
-                      
-                      ary.push (obj);
-                    }
-
-                    let newArry = arry;
-                    
-                    $vmc.issues = ary;
-                    return newArry;
-
-                  })
-                  .then (resp => {
-                    for (let i = 0; i < resp.length; i++) {
-                      let object = resp[i];
-                      
-                    }
-                  })
-                  
-              })
+            $vmc.issueOpenedCount = opened;
+            $vmc.issueClosedCount = all - opened;
+            $vmc.issues = ary;
 
           })
       },
+      
+      // showIssue () {
+      //   let $vmc = this;
+      //   let Org = Parse.Object.extend ('Organization');
+      //   let query = new Parse.Query (Org);
+      //   let ary = [];
+      //   query.equalTo ('memberId', $vmc.user.userId);
+      //   query.find ()
+      //     .then (resp => {
+      //       for (let i = 0; i < resp.length; i ++) {
+      //         let object = resp[i];
+      //         ary.push (object.id);
+      //       }
+
+      //       return ary;
+      //     })
+      //     .then (resp => {
+      //       let ary = [];
+      //       let orgId = resp;
+      //       let Project = Parse.Object.extend ('Project');
+      //       let query = new Parse.Query (Project);
+
+      //       query.containedIn ('orgId', resp);
+      //       query.find ()
+      //         .then (resp => {
+      //           for (let i = 0; i < resp.length; i ++) {
+      //             let object = resp[i];
+
+      //             ary.push (object.id)
+      //           }
+
+      //           return ary;
+      //         })
+      //         .then (resp => {
+      //           let ary = [];
+      //           let arry = [];
+      //           let Issue = Parse.Object.extend ('Issue');
+      //           let query = new Parse.Query (Issue);
+
+      //           query.containedIn ('proId', resp);
+      //           query.find ()
+      //             .then (resp => {
+                    
+      //               for (let i = 0; i < resp.length; i ++) {
+      //                 let obj = {};
+      //                 let object = resp[i];
+      //                 obj.issueId = object.id;
+      //                 obj.orgId = object.get ('orgId');
+      //                 obj.proId = object.get ('proId');
+      //                 obj.proName = object.get ('proName');
+      //                 obj.orgName = object.get ('orgName');
+      //                 obj.issueName = object.get ('name');
+      //                 obj.issueOpened = object.get ('issueOpened');
+      //                 obj.creator = object.get ('creator');
+
+      //                 if (obj.issueOpened === true) {
+      //                   $vmc.openedCount += 1;
+      //                   arry.push (obj);
+      //                 } else {
+      //                   $vmc.closedCount += 1;
+      //                 }
+                      
+      //                 ary.push (obj);
+      //               }
+
+      //               let newArry = arry;
+                    
+      //               $vmc.issues = ary;
+      //               return newArry;
+
+      //             })
+                  
+      //         })
+
+      //     })
+      // },
 
     },
 
     watch: {
       user () {
-        this.showIssue ();
-        this.showAssignee ();
-        this.showCreator ();
+        // this.showIssue ();
+        // this.showAssignee ();
       }
     }
   }
