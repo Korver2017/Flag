ff29139b7f733a8fcb3c4d440d5b0d949f2f5133
Korver Huang <korver2017@gmail.com>
2019-06-17 10:27:38 +0800

    Trim milestone-component

--- src/views/milestone.vue
+++ src/views/milestone.vue
@@ -113,196 +113,116 @@
 
       </div>
 
+      <div class="container my-4">
+        
+        <ul v-for="(mile, index) in milestones" class="list-group list-group-flush">
 
+          <li v-if="mile.mileOpened === true && showOpened === true" class="text-left list-group-item">
 
-    <div class="container my-4">
-      
-      <ul v-for="(mile, index) in milestones" class="list-group list-group-flush">
+              <div class="row">
+                
+                <div class="col-6">
 
-        <li v-if="mile.mileOpened === true && showOpened === true" class="text-left list-group-item">
-
-            <div class="row">
-              
-              <div class="col-6">
-
-                <router-link v-if="milestones" :to="{ name: 'milestone-detail', params: { mileId: mile.mileId } }" class="text-decoration-none" tag="a" active-class="active">
-                  <h3 class="">{{ mile.title }}</h3>
-                </router-link>
-
-                <div class="row text-left">
-
-                  <div v-if="milestones[index].dueDate !== '' || undefined || null" class="mr-3">{{ milestones[index].dueDate }}</div>
-                  
-                  <!-- <p v-if="milestones[index].mileDesc !== ''">{{ milestones[index].mileDesc }}</p> -->
-
-                  <div class="mr-3">{{ milestones[index].open }} 個開啟中</div>
-                  <div class="">{{ milestones[index].closed }} 個已關閉</div>
-
-                </div>
-
-              </div>
-
-              <div class="col-6">
-
-                <div class="progress">
-                  <div class="progress-bar bg-success" role="progressbar" :style="{ width: mile.percentage + '%'}" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-
-                <div class="mt-2 text-right">
-
-                  <router-link class="mr-3 btn btn-success" :to="{ name: 'edit-milestone', params: { mileId: milestones[index].mileId } }" tag="button" active-class="active">
-                    編輯
+                  <router-link v-if="milestones" :to="{ name: 'milestone-detail', params: { mileId: mile.mileId } }" class="text-decoration-none" tag="a" active-class="active">
+                    <h3 class="">{{ mile.title }}</h3>
                   </router-link>
 
-                  <button v-if="milestones[index].mileOpened === true && milestones[index].mileEditing === false" class="mr-3 btn btn-warning" @click.prevent="closeMilestone (index)">關閉</button>
+                  <div class="row text-left">
 
-                  <button v-else-if="milestones[index].mileOpened === false && milestones[index].mileEditing === false" class="mr-3 btn btn-primary" @click.prevent="reopenMilestone (index)">重新開啟</button>
+                    <div v-if="milestones[index].dueDate !== '' || undefined || null" class="mr-3">{{ milestones[index].dueDate }}</div>
 
-                  <button v-if="milestones[index].mileEditing === false" class="mr-3 btn btn-danger" @click="deleteMile (index)">刪除</button>
+                    <div class="mr-3">{{ milestones[index].open }} 個開啟中</div>
+                    <div class="">{{ milestones[index].closed }} 個已關閉</div>
+
+                  </div>
+
+                </div>
+
+                <div class="col-6">
+
+                  <div class="progress">
+                    <div class="progress-bar bg-success" role="progressbar" :style="{ width: mile.percentage + '%'}" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+
+                  <div class="mt-2 text-right">
+
+                    <router-link class="mr-3 btn btn-success" :to="{ name: 'edit-milestone', params: { mileId: milestones[index].mileId } }" tag="button" active-class="active">
+                      編輯
+                    </router-link>
+
+                    <button v-if="milestones[index].mileOpened === true && milestones[index].mileEditing === false" class="mr-3 btn btn-warning" @click.prevent="closeMilestone (index)">關閉</button>
+
+                    <button v-else-if="milestones[index].mileOpened === false && milestones[index].mileEditing === false" class="mr-3 btn btn-primary" @click.prevent="reopenMilestone (index)">重新開啟</button>
+
+                    <button v-if="milestones[index].mileEditing === false" class="mr-3 btn btn-danger" @click="deleteMile (index)">刪除</button>
+
+                  </div>
 
                 </div>
 
               </div>
 
-            </div>
 
+          </li>
 
-        </li>
+          <li v-if="mile.mileOpened === false && showOpened === false" class="py-5 text-left list-group-item">
 
-        <li v-if="mile.mileOpened === false && showOpened === false" class="py-5 text-left list-group-item">
+                        <div class="row">
+                
+                <div class="col-6">
 
-                      <div class="row">
-              
-              <div class="col-6">
-
-                <router-link v-if="milestones" :to="{ name: 'milestone-detail', params: { mileId: mile.mileId } }" class="text-decoration-none" tag="a" active-class="active">
-                  <h3 class="">{{ mile.title }}</h3>
-                </router-link>
-
-                <div class="row text-left">
-
-                  <div v-if="milestones[index].dueDate !== '' || undefined || null" class="mr-3">{{ milestones[index].dueDate }}</div>
-                  
-                  <!-- <p v-if="milestones[index].mileDesc !== ''">{{ milestones[index].mileDesc }}</p> -->
-
-                  <div class="mr-3">{{ milestones[index].open }} 個開啟中</div>
-                  <div class="">{{ milestones[index].closed }} 個已關閉</div>
-
-                </div>
-
-              </div>
-
-              <div class="col-6">
-
-                <div class="progress">
-                  <div class="progress-bar bg-success" role="progressbar" :style="{ width: mile.percentage + '%'}" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-
-                <div class="mt-2 text-right">
-
-                  <router-link class="mr-3 btn btn-success" :to="{ name: 'edit-milestone', params: { mileId: milestones[index].mileId } }" tag="button" active-class="active">
-                    編輯
+                  <router-link v-if="milestones" :to="{ name: 'milestone-detail', params: { mileId: mile.mileId } }" class="text-decoration-none" tag="a" active-class="active">
+                    <h3 class="">{{ mile.title }}</h3>
                   </router-link>
 
-                  <button v-if="milestones[index].mileOpened === true && milestones[index].mileEditing === false" class="mr-3 btn btn-warning" @click.prevent="closeMilestone (index)">關閉</button>
+                  <div class="row text-left">
 
-                  <button v-else-if="milestones[index].mileOpened === false && milestones[index].mileEditing === false" class="mr-3 btn btn-primary" @click.prevent="reopenMilestone (index)">重新開啟</button>
+                    <div v-if="milestones[index].dueDate !== '' || undefined || null" class="mr-3">{{ milestones[index].dueDate }}</div>
 
-                  <button v-if="milestones[index].mileEditing === false" class="mr-3 btn btn-danger" @click="deleteMile (index)">刪除</button>
+                    <div class="mr-3">{{ milestones[index].open }} 個開啟中</div>
+                    <div class="">{{ milestones[index].closed }} 個已關閉</div>
+
+                  </div>
+
+                </div>
+
+                <div class="col-6">
+
+                  <div class="progress">
+                    <div class="progress-bar bg-success" role="progressbar" :style="{ width: mile.percentage + '%'}" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+
+                  <div class="mt-2 text-right">
+
+                    <router-link class="mr-3 btn btn-success" :to="{ name: 'edit-milestone', params: { mileId: milestones[index].mileId } }" tag="button" active-class="active">
+                      編輯
+                    </router-link>
+
+                    <button v-if="milestones[index].mileOpened === true && milestones[index].mileEditing === false" class="mr-3 btn btn-warning" @click.prevent="closeMilestone (index)">關閉</button>
+
+                    <button v-else-if="milestones[index].mileOpened === false && milestones[index].mileEditing === false" class="mr-3 btn btn-primary" @click.prevent="reopenMilestone (index)">重新開啟</button>
+
+                    <button v-if="milestones[index].mileEditing === false" class="mr-3 btn btn-danger" @click="deleteMile (index)">刪除</button>
+
+                  </div>
 
                 </div>
 
               </div>
 
-            </div>
+          </li>
 
-          <!-- <div v-if="milestones[index].mileEditing === false"> -->
+        </ul>
 
-            <!-- <router-link v-if="milestones" :to="{ name: 'milestone-detail', params: { mileId: mile.mileId, mileTitle: mile.title }}" class="text-decoration-none" tag="a" active-class="active">
-              <h3 class="my-3">{{ mile.title }}</h3>
-            </router-link> -->
-
-            <!-- <div class="row">
-
-              <div class="col-6">
-
-                <p v-if="milestones[index].dueDate !== ''">Due Date by {{ milestones[index].dueDate }}</p>
-                <p v-if="milestones[index].mileDesc !== ''">{{ milestones[index].mileDesc }}</p>
-
-              </div>
-
-              <div class="col-6">
-
-                <div class="mx-auto row">
-                  <p class="mx-2">{{ mile.percentage }}% Complete</p>
-                  <p class="mx-2">{{ mile.open }} open</p>
-                  <p class="mx-2">{{ mile.closed }} closed</p>
-                </div>
-
-                <div class="progress">
-                  <div class="progress-bar bg-success" role="progressbar" :style="{ width: mile.percentage + '%'}" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-
-              </div>
-
-              <div class="text-right mb-5 ml-auto mr-5">
-
-                <div class="text-right mt-3">
-
-                  <button v-if="milestones[index].mileEditing === false" class="mr-3 btn btn-success" @click.prevent="milestones[index].mileEditing = true">Edit</button>
-                  <button v-if="milestones[index].mileOpened === true && milestones[index].mileEditing === false" class="mr-3 btn btn-warning" @click.prevent="closeMilestone (index)">Close</button>
-                  <button v-else-if="milestones[index].mileOpened === false && milestones[index].mileEditing === false" class="mr-3 btn btn-primary" @click.prevent="reopenMilestone (index)">Reopen</button>
-                  <button v-if="milestones[index].mileEditing === false" @click="deleteMile (index)" class="mr-3 btn btn-danger">Delete</button>
-
-                </div>
-
-              </div>
-
-            </div>
-
-          </div>
-
-          <div v-else class="col-8 mx-auto form-group text-left">
-
-            <label>Milestone Title</label>
-            <input v-model.trim="milestones[index].title" class="mb-3 form-control">
-
-            <label>Due Date (Optional)</label>
-            <date-picker v-model="milestones[index].dueDate" :config="options"></date-picker>
-
-            <label class="mt-3" for="description">Description</label>
-            <textarea v-model="milestones[index].mileDesc" class="form-control mb-3" rows="10" id="description"></textarea>
-
-            <div class="text-center">
-
-              <button class="mx-1 btn btn-success" @click.prevent="updateMile (index)">Update</button>
-              <button class="mx-1 btn btn-danger" @click.prevent="cancelEditing (index)">Cancel</button>
-
-            </div>
-
-          </div> -->
-
-        </li>
-
-      </ul>
+      </div>
 
     </div>
-
-    </div>
-
 
 </template>
 
 <script>
   
   import Parse from "parse";
-  // import DatetimePicker from "@/components/datetime-picker.vue";
-  
-  // Import this component
-  import datePicker from 'vue-bootstrap-datetimepicker';
-  
-  // Import date picker css
-  import 'pc-bootstrap4-datetimepicker/build/css/bootstrap-datetimepicker.css';
 
   export default {
 
@@ -311,34 +231,24 @@
 
 
     components: {
-      datePicker
     },
 
 
     data () {
       return {
-        date: '',
-        options: {
-          format: 'DD-MM-YYYY',
-          useCurrent: false,
-        },
-        addingMile: false,
-        mileDesc: '',
         milestones: [],
         showOpened: true,
-        mileTitle: '',
         mileOpened: 0,
         mileClosed: 0,
-        // orgId: '',
         orgName: '',
         proName: '',
         mileIssues: [],
-        editingMile: false,
       }
     },
 
 
     computed: {
+
       proId () {
         return this.$route.params.proId; 
       },
@@ -351,23 +261,6 @@
 
 
     created () {
-      // Add Milestone
-      
-      // const Mile = Parse.Object.extend("Milestone");
-      // const mile = new Mile();
-
-      // mile.set("title", 'M1');
-      // mile.set("proId", this.proId);
-
-      // mile.save()
-      // .then((mile) => {
-      //   // Execute any logic that should take place after the object is saved.
-      //   alert('New object created with objectId: ' + mile.id);
-      // }, (error) => {
-      //   // Execute any logic that should take place if the save fails.
-      //   // error is a Parse.Error with an error code and message.
-      //   alert('Failed to create new object, with error code: ' + error.message);
-      // });
     },
 
 
@@ -382,7 +275,6 @@
     methods: {
       showRouteName () {
         let $vmc = this;
-
         let Project = Parse.Object.extend ('Project');
         let query = new Parse.Query (Project);
 
@@ -464,58 +356,6 @@
       },
 
 
-      // addMilestone () {
-      //   let $vmc = this;
-
-      //   const Mile = Parse.Object.extend ('Milestone');
-      //   const mile = new Mile ();
-
-      //   mile.set ('title', $vmc.mileTitle);
-      //   mile.set ('proId', $vmc.proId);
-      //   mile.set ('orgId', $vmc.orgId);
-      //   mile.set ('mileOpened', true);
-      //   mile.set ('dueDate', $vmc.date);
-      //   mile.set ('mileDesc', $vmc.mileDesc);
-
-      //   mile.save ()
-      //     .then (resp => {
-      //       $vmc.mileTitle = '';
-      //       $vmc.showMile ();
-      //       $vmc.cancelAddMile ();
-      //       // $vmc.showMile ();
-      //       // Execute any logic that should take place after the object is saved.
-      //     }, (error) => {
-      //       // Execute any logic that should take place if the save fails.
-      //       // error is a Parse.Error with an error code and message.
-      //       alert ('Failed to create new object, with error code: ' + error.message);
-      //     });
-      // },
-
-
-      cancelAddMile () {
-        let $vmc = this;
-        $vmc.mileTitle = '';
-        $vmc.date = '';
-        $vmc.addingMile = false;
-      },
-
-
-      editMilestone (index) {
-        let $vmc = this;
-        let Mile = Parse.Object.extend ('Milestone');
-        let query = new Parse.Query (Mile);
-        query.get ($vmc.milestones[index].mileId)
-          .then (resp => {
-            resp.set ('title', $vmc.milestones[index].newTitle);
-            resp.save ()
-              .then (() => {
-                $vmc.milestones[index].newTitle = '';
-                $vmc.showMile ();
-              })
-          })
-      },
-
-
       closeMilestone (index) {
         let $vmc = this;
         let Mile = Parse.Object.extend ('Milestone');
@@ -548,27 +388,6 @@
       },
 
 
-      updateMile (index) {
-        let $vmc = this;
-        let mileId = $vmc.milestones[index].mileId;
-        let Mile = Parse.Object.extend ('Milestone');
-        let query = new Parse.Query (Mile);
-
-        query.get (mileId)
-          .then (resp => {
-            // Now let's update it with some new data. In this case, only cheatMode and score
-            // will get sent to the cloud. playerName hasn't changed.
-            resp.set ('title', $vmc.milestones[index].title);
-            resp.set ('dueDate', $vmc.milestones[index].dueDate);
-            resp.set ('mileDesc', $vmc.milestones[index].mileDesc);
-            resp.save ()
-              .then (resp => {
-                $vmc.cancelEditing (index);
-              })
-          })
-      },
-
-
       deleteMile (index) {
         let $vmc = this;
         let mileId = $vmc.milestones[index].mileId;
@@ -577,8 +396,6 @@
 
         query.get (mileId)
           .then (resp => {
-            // Now let's update it with some new data. In this case, only cheatMode and score
-            // will get sent to the cloud. playerName hasn't changed.
             if (confirm ('確定要刪除此 Milestone 嗎？')) {
               resp.destroy ()
                 .then (resp => {
@@ -593,45 +410,8 @@
             else return;
           })
       },
-
-
-      cancelEditing (index) {
-        let $vmc = this;
-        let mileId = $vmc.milestones[index].mileId;
-        let Mile = Parse.Object.extend ('Milestone');
-        let query = new Parse.Query (Mile);
-
-        query.get (mileId)
-          .then (resp => {
-            $vmc.milestones[index].title = resp.get ('title');
-            $vmc.milestones[index].dueDate = resp.get ('dueDate');
-            $vmc.milestones[index].mileDesc = resp.get ('mileDesc');
-          })
-          .then (resp => {
-            $vmc.milestones[index].mileEditing = false;
-          });
-      },
-
-
-      showMileDetail (mileId) {
-        let $vmc = this;
-        let Issue = Parse.Object.extend ('Issue');
-        let query = new Parse.Query (Issue);
-        let ary = [];
-        query.equalTo ('milestone', mileId);
-        query.find()
-          .then (resp => {
-            for (let i = 0; i < resp.length; i++) {
-              let object = resp[i];
-              ary.push (object.get ('name'));
-            }
-          })
-
-        $vmc.mileIssues = ary;
-        
-      }
-    },
     
+    }
   }
 </script>
 
