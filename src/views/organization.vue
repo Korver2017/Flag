605c2ae782a997f6a34fd4c70df54b1bfff2a574
Korver Huang <korver2017@gmail.com>
2019-06-17 10:41:46 +0800

    Trim organization-component

--- src/views/organization.vue
+++ src/views/organization.vue
@@ -16,8 +16,6 @@
 
         <div class="text-right">
 
-          <!-- <button class="text-right btn btn-success" @click="newProject">新增儲存庫</button> -->
-
           <router-link class="btn btn-success" to="/project/create" tag="button" active-class="active">
             新增儲存庫
           </router-link>
@@ -76,9 +74,8 @@
         <div class="py-3 bg-light-theme border-light-theme border-bottom-0">
 
           <div class="row px-4">
-          
+        
             <h5 class="mb-0 pl-2">組織成員</h5>
-
             <div class="ml-auto mr-4">{{ users.length }}</div>
 
           </div>
@@ -98,29 +95,23 @@
         <div class="mt-5 py-3 bg-light-theme border-light-theme border-bottom-0">
 
           <div class="row px-4">
-          
             <h5 class="mb-0 pl-2">組織團隊</h5>
-
-            <!-- <div class="ml-auto">{{ users.length }}</div> -->
-
           </div>
           
         </div>
 
         <div class="border-light-theme border-bottom-0">
 
-            <div class="py-3 ml-3">
-              {{ users.length }} 名成員．{{ projects.length }} 個儲存庫
-            </div>
+          <div class="py-3 ml-3">
+            {{ users.length }} 名成員．{{ projects.length }} 個儲存庫
+          </div>
           
         </div>
 
         <div class="border-light-theme">
 
           <div class="py-3 ml-3">
-
             <button class="btn btn-primary" @click="">建立團隊</button>
-            
           </div>
           
         </div>
@@ -145,7 +136,6 @@
     data () {
       return {
         orgName: '',
-        projectName: '',
         projects: [],
         defaultLabels: [],
         users: '',
@@ -262,40 +252,6 @@
           $vmc.projects = ary;
         });
       },
-
-
-      newProject () {
-        let $vmc = this;
-        let Pro = Parse.Object.extend ('Project');
-        let pro = new Pro ();
-
-        pro.set ('name', $vmc.projectName);
-        pro.set ('orgId', $vmc.orgId);
-        pro.set ('orgName', $vmc.orgName);
-        pro.save ()
-          .then (resp => {
-            $vmc.showProject ();
-            let proId = resp.id;
-            const Label = Parse.Object.extend ('Label');
-            let len = $vmc.defaultLabels.length
-            for (let i = 0; i < len; i ++) {
-              let label = new Label ();
-              let object = $vmc.defaultLabels[i];
-
-              label.set ('title', object.title);
-              label.set ('labelDesc', object.labelDesc);
-              label.set ('proId', proId);
-              label.save ();
-            }
-
-            }, (error) => {
-              // Execute any logic that should take place if the save fails.
-              // error is a Parse.Error with an error code and message.
-              alert('Failed to create new object, with error code: ' + error.message);
-            });
-
-        $vmc.projectName = '';
-      }
 
     },
 
